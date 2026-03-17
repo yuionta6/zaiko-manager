04ef8e4c9795c66c4926929a6d7243176cd76580
liontaro <106938523+yuionta6@users.noreply.github.com>
2026-03-17 12:05:28 +0900

    環境変数に対応

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// あなたのFirebase鍵情報（スクリーンショットのもの）
 const firebaseConfig = {
-  apiKey: "AIzaSyDyJNCZ6h0yfd0NfbYzkv7vyTc7HtTu5pU",
-  authDomain: "zaiko-manager-c6c48.firebaseapp.com",
-  projectId: "zaiko-manager-c6c48",
-  storageBucket: "zaiko-manager-c6c48.firebasestorage.app",
-  messagingSenderId: "215532467012",
-  appId: "1:215532467012:web:af70c0ef12dc4784a4dd11",
-  measurementId: "G-84WYDDH2EW"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Firebaseの初期化
 const app = initializeApp(firebaseConfig);
-
-// 【ここが重要！】dbを「export」することで、他のファイルから使えるようになります
 export const db = getFirestore(app);
